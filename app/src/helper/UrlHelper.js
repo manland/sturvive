713dad76ecc403d286edea5f6c2296ddbb7db3a1
manland <rmaneschi@gmail.com>
2013-12-31 13:51:24 +0100

    delete rule 127.0.0.1 to test in semi prod mode

--- app/src/helper/UrlHelper.js
+++ app/src/helper/UrlHelper.js
@@ -2,7 +2,6 @@ define('UrlHelper', function() {
 
   var isDev = 
     document.URL.indexOf('localhost') !== -1 || 
-    document.URL.indexOf('127.0.0.1') !== -1 || 
     document.URL.indexOf('debug') !== -1;
 
   console.log(document.URL, isDev);
